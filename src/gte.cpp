8e9bac761827cac7a7665ad5d151d783ff31eb87
Daniel Shimmyo <dshimmyo@gmail.com>
2026-07-31 11:21:41 -0400

    fairly robust wasm gamepad and joystick paddle

--- src/gte.cpp
+++ src/gte.cpp
@@ -1130,10 +1130,12 @@ extern "C" {
 			}
 		}
 		void SetJoystickPaddleMode(bool enabled) {
-			paddleDetected = enabled;//allows ROM to adjust
+			paddleDetected = enabled;//allows BrickGame ROM to adjust
 		}
 		void SetJoystickPaddleButton(bool enabled){
-			joysticks->SetPaddleAButtonDirect(enabled);
+			if (joysticks != nullptr) {
+				joysticks->SetPaddleAButtonDirect(enabled);
+			}
 		}
 		void SetJoystickHeldButtons(int buttonmask) {
 			if (joysticks != nullptr) {
@@ -1161,7 +1163,9 @@ extern "C" {
 
 		void EMSCRIPTEN_KEEPALIVE UpdatePaddleFromMouseJS(int index, int dx) {
 			// Calls your existing logic that adds dx to the current paddle position
-			joysticks->UpdatePaddleFromMouse(0, dx);
+			if (joysticks != nullptr) {
+				joysticks->UpdatePaddleFromMouse(0, dx);
+			}
 		}
 
 	}
